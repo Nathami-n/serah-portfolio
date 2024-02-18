468f86b37052fb575ff042f83a43cafd0bf3d539
Nathan George <georgenathan010@gmail.com>
2024-02-18 11:50:19 +0300

    extended color theme

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = ({children}) => {
+  return (
+   <main className="flex bg-primary  min-h-screen">
+    {children}
+    <Hero/>
+   </main>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#160F0E'
+      }
+    },
   },
-  plugins: [import('daisyui')],
+  plugins: [],
 }
