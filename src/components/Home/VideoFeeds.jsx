67e6ae783e8d2ca5722f9586fa12e5c8de5d9dbe
Nathan George <georgenathan010@gmail.com>
2024-02-22 14:38:01 +0300

    fixed the mobile responsiveness

--- src/components/Home/VideoFeeds.jsx
+++ src/components/Home/VideoFeeds.jsx
@@ -19,7 +19,7 @@ const VideoFeeds = ({ videoUrl }) => {
 
   return (
     videoUrl ? (
-    <div className=" md:gap-4 md:p-5 max-md:flex  max-lg:mt-10 max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] md:grid md:grid-cols-4 lg:grid-cols-4  xl:grid-cols-5 shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  overflow-y-scroll max-sm:p-9 max-sm:gap-5 max-sm:overflow-x-hidden">
+    <div className=" md:gap-4 md:p-5 max-md:flex  max-lg:mt-10 max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] md:grid md:grid-cols-4 lg:grid-cols-4  xl:grid-cols-5 shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  max-sm:p-12 max-sm:gap-5 max-sm:overflow-x-hidden max-sm:mx-auto">
       <div className=" col-span-3 row-span-2 xl:col-span-4 ">
         <div className="w-full h-full max-sm:rounded-full">
           <ReactPlayer url={videoUrl} width="100%" height="100%" />
@@ -31,7 +31,7 @@ const VideoFeeds = ({ videoUrl }) => {
       })}
     </div>)
     : (
-      <div className=" md:gap-4  max-lg:grid max-lg:grid-cols-2  lg:grid lg:grid-cols-3 max-lg:mt-12 md:p-5 max-md:flex max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  overflow-y-scroll max-sm:p-9 max-sm:gap-5 max-sm:overflow-x-hidden">
+      <div className=" md:gap-4   max-lg:grid max-lg:grid-cols-2 md:pt-12 lg:grid lg:grid-cols-4 max-lg:mt-12 md:p-5 max-md:flex max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]   max-sm:p-12 max-sm:gap-5 max-sm:overflow-x-hidden max-sm:mx-auto">
       {videoData?.map((video, idx) => {
         return <VideoCard key={idx} video={video} />;
       })}
