3c74fe366095fd2c7de418f8916c528977d2e02c
Nathan George <georgenathan010@gmail.com>
2024-02-22 14:16:06 +0300

    finished on the responsiveness of the video player

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import VideoFeeds from "./Home/VideoFeeds";
 
 const Hero = ({ videoUrl }) => {
   return (
-    <div className="bg-primary">
+    <div className="bg-primary overflow-x-hidden">
       <VideoFeeds videoUrl={videoUrl} />
     </div>
   );
--- src/components/Home/VideoFeeds.jsx
+++ src/components/Home/VideoFeeds.jsx
@@ -16,9 +16,11 @@ const VideoFeeds = ({ videoUrl }) => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
   return (
-    <div className=" md:gap-4 md:p-5 max-md:flex max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] md:grid md:grid-cols-4 lg:grid-cols-4  xl:grid-cols-5 shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  overflow-y-scroll max-sm:p-9 max-sm:gap-5 max-sm:overflow-x-hidden">
-      <div className=" col-span-3 row-span-2 xl:col-span-4 max-sm:fixed max-sm:top-50 max-sm:w-[50px] max-sm:h-[50px] max-sm:z-[89] max-sm:right-9 max-sm:rounded-full">
+    videoUrl ? (
+    <div className=" md:gap-4 md:p-5 max-md:flex  max-lg:mt-10 max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] md:grid md:grid-cols-4 lg:grid-cols-4  xl:grid-cols-5 shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  overflow-y-scroll max-sm:p-9 max-sm:gap-5 max-sm:overflow-x-hidden">
+      <div className=" col-span-3 row-span-2 xl:col-span-4 ">
         <div className="w-full h-full max-sm:rounded-full">
           <ReactPlayer url={videoUrl} width="100%" height="100%" />
         </div>
@@ -27,7 +29,14 @@ const VideoFeeds = ({ videoUrl }) => {
       {videoData?.map((video, idx) => {
         return <VideoCard key={idx} video={video} />;
       })}
+    </div>)
+    : (
+      <div className=" md:gap-4  max-lg:grid max-lg:grid-cols-2  lg:grid lg:grid-cols-3 max-lg:mt-12 md:p-5 max-md:flex max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  overflow-y-scroll max-sm:p-9 max-sm:gap-5 max-sm:overflow-x-hidden">
+      {videoData?.map((video, idx) => {
+        return <VideoCard key={idx} video={video} />;
+      })}
     </div>
+    )
   );
 };
 
