aeb965f52d3477e908e5444bbc3e6414216dcfd1
Nathan George <georgenathan010@gmail.com>
2024-02-22 12:21:18 +0300

    fixed the view of the player on mobile

--- src/components/Home/VideoFeeds.jsx
+++ src/components/Home/VideoFeeds.jsx
@@ -7,8 +7,8 @@ const VideoFeeds = ({ videoUrl }) => {
   const [videoData, setVideoData] = useState(data);
   return (
     <div className=" md:gap-4 md:p-5 max-md:flex max-md:flex-wrap max-md:justify-center max-md:m-20 max-sm:w-[400px] md:grid md:grid-cols-4 lg:grid-cols-4  xl:grid-cols-5 shadow-lg shadow-slate-600 rounded-lg border border-[#5f1803] bg-[#0e0401]  overflow-y-scroll max-sm:p-9 max-sm:gap-5 max-sm:overflow-x-hidden">
-      <div className=" col-span-3 row-span-2 xl:col-span-4 max-md:fixed max-md:top-0 max-md:w-full max-md:h-[400px] max-md:z-[89]">
-        <div className="w-full h-full">
+      <div className=" col-span-3 row-span-2 xl:col-span-4 max-sm:fixed max-sm:top-0 max-sm:w-[200px] max-sm:h-[200px] max-sm:z-[89] max-sm:right-8 max-sm:rounded-full">
+        <div className="w-full h-full max-sm:rounded-full">
           <ReactPlayer url={videoUrl} width="100%" height="100%" />
         </div>
       </div>
