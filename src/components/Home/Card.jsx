79ee1ec795d23be6551fbbdda2d200c8dd95ad53
Nathan George <georgenathan010@gmail.com>
2024-02-23 17:32:09 +0300

    added blurhash

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,14 +1,31 @@
-const Card = ({ img: { imgSrc, description } }) => {
+import { useEffect, useState } from "react";
+import { Blurhash } from "react-blurhash";
+
+const Card = ({ img: { imgSrc, description,hash } }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setIsLoaded(true);
+    };
+    img.src = imgSrc;
+  }, [imgSrc]);
+
   return (
     <div className="group relative max-md:h-[300px] max-md:w-[300px] md:h-[400px] md:w-[400px] overflow-hidden bg-neutral-200">
-      <div
-        style={{
-          backgroundImage: `url(${imgSrc})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="absolute inset-0 z-0 transition-transform duration-600 group-hover:scale-110 "
-      ></div>
+      <div className="absolute inset-0 z-0 transition-transform duration-600 group-hover:scale-110 ">
+
+        {
+          isLoaded ? (<img src={imgSrc} className="object-cover h-full w-full" />) : (
+            <Blurhash 
+            width={300}
+            height={300}
+            hash={`${hash}`}
+            punch={1}
+            />
+          )
+        }
+      </div>
       <div className="absolute inset-0 z-10 grid place-content-center">
         <p className="bg-gradient-to-br from-white/20 to-white/0 p-8  max-md:text-2xl md:text-4xl font-black uppercase text-white backdrop-blur-lg">
           {description}
--- src/utils/imageData.jsx
+++ src/utils/imageData.jsx
@@ -3,30 +3,36 @@ export const imgData = [
     id: 1,
     imgSrc: "/awards/kalonzo.jpg",
     description: "With Kalonzo",
+    hash: "LVJ89P9EIV01~qIoD%IU%Laet7V@",
   },
   {
     id: 2,
     imgSrc: "/awards/kalonzo2.jpg",
     description: "With Kalonzo",
+    hash: "LWKJ[F?b={tl~WT0nhNa?HNHIUX8",
   },
   {
     id: 3,
     imgSrc: "/awards/trophy.jpg",
     description: "Trophy",
+    hash: "LIF~8K%$_Nt700R44:i_ITV@%0oz",
   },
   {
     id: 4,
     imgSrc: "/awards/Machoka.jpg",
     description: " With Machoka",
+    hash: "LA9jJ-}@Qlaw9Fs:-=g3EgADIo$+",
   },
   {
     id: 5,
     imgSrc: "/awards/serahDancing.jpg",
     description: "Carnival",
+    hash: "LGCOUm+|M|}[IT%M-WMxF2RiNZOZ",
   },
   {
     id: 8,
     imgSrc: "/awards/carnival3.jpg",
     description: "Perfoming",
-  }
+    hash: "L9Aa=3xNEIRq2OWa$mWlIjEtt2X3",
+  },
 ];
