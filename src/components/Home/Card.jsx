b8a73ce274173a9a0cdb65fd60831b39f1202d8e
Nathan George <georgenathan010@gmail.com>
2024-02-23 16:23:33 +0300

    dynamically changed the responsiveness of the images

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ img: { imgSrc, description } }) => {
   return (
-    <div className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200">
+    <div className="group relative max-md:h-[300px] max-md:w-[300px] md:h-[400px] md:w-[400px] overflow-hidden bg-neutral-200">
       <div
         style={{
           backgroundImage: `url(${imgSrc})`,
@@ -10,7 +10,7 @@ const Card = ({ img: { imgSrc, description } }) => {
         className="absolute inset-0 z-0 transition-transform duration-600 group-hover:scale-110 "
       ></div>
       <div className="absolute inset-0 z-10 grid place-content-center">
-        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
+        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8  max-md:text-2xl md:text-4xl font-black uppercase text-white backdrop-blur-lg">
           {description}
         </p>
       </div>
--- src/components/Home/HomeInterlude.jsx
+++ src/components/Home/HomeInterlude.jsx
@@ -26,7 +26,7 @@ const HomeInterlude = () => {
         <span className="font-semibold uppercase text-neutral-500">
           <motion.button
             animate={{ y: isAnimating ? [0, 10] : 0 }}
-            className="p-5 bg-[#14142b] text-white rounded-full w-[250px] hover:bg-gray-100 transition-all hover:shadow-lg hover:text-black hover:duration-300 hover:delay-1 md:mt-4"
+            className="p-5 bg-[#14142b] text-white rounded-full w-[250px] text-3xl hover:bg-gray-100 transition-all hover:shadow-lg hover:text-black hover:duration-300 hover:delay-1 md:mt-4"
             transition={{ duration: 1, yoyo: Infinity }}
           >
             More About me
